4849952e6cfc7803d02aee91e7824cbff2649671
Stephen <stephenbrady365@gmail.com>
2021-05-17 21:37:46 +0100

    Add <Graph> and <Converter> to <App>

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,7 @@
 import { Header } from "../components/Header";
 import { Layout } from "../components/Layout";
+import { Converter } from "../converter/Converter";
+import { Graph } from "../graph/Graph";
 import { RateList } from "../rates-list/RateList";
 
 function App() {
@@ -8,6 +10,8 @@ function App() {
       <Header headingSize="h1" headingText="Forex Dashboard" />
       <Layout>
         <RateList />
+        <Graph />
+        <Converter />
       </Layout>
     </>
   );
