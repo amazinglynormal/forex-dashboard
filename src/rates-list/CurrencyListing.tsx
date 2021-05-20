a566b7bc0818a2b72cb829cb63d19efaccf55ea6
Stephen <stephenbrady365@gmail.com>
2021-05-20 10:51:41 +0100

    Add <CurrencyListing>

---
+++ src/rates-list/CurrencyListing.tsx
@@ -0,0 +1,36 @@
+import styles from "./currency-listing.module.css";
+
+import { ArrowUp } from "../components/ArrowUp";
+import { ArrowDown } from "../components/ArrowDown";
+import { Equals } from "../components/Equals";
+
+interface Props {
+  symbol: String;
+  fullName: String;
+  trend: "up" | "down" | "equal";
+  latestRate: number;
+}
+
+export const CurrencyListing = ({
+  symbol,
+  fullName,
+  trend,
+  latestRate,
+}: Props) => {
+  const trendSymbols = {
+    up: <ArrowUp />,
+    down: <ArrowDown />,
+    equal: <Equals />,
+  };
+
+  return (
+    <li className={styles.listing}>
+      <div className={styles.listingLayout}>
+        <span className={styles.symbol}>{symbol}</span>
+        <span className={styles.fullName}>{fullName}</span>
+        <span className={styles.trend}>{trendSymbols[trend]}</span>
+        <span className={styles.latestRate}>{latestRate}</span>
+      </div>
+    </li>
+  );
+};
