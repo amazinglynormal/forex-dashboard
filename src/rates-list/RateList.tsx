7a053f864701e60e002e97c8e10d6b4b5587dc9f
Stephen <stephenbrady365@gmail.com>
2021-05-20 10:52:47 +0100

    Add basic working <RateList>

--- src/rates-list/RateList.tsx
+++ src/rates-list/RateList.tsx
@@ -1,7 +1,25 @@
+import styles from "./rate-list.module.css";
+import useRateData from "../hooks/useRateData";
+import { CurrencyListing } from "./CurrencyListing";
+
 export const RateList = () => {
+  const rateData = useRateData();
+
+  const renderedCurrencies = rateData.map((data) => {
+    return (
+      <CurrencyListing
+        key={data.symbol}
+        symbol={data.symbol}
+        fullName={data.fullName}
+        trend={data.trend}
+        latestRate={data.latestRate}
+      />
+    );
+  });
+
   return (
-    <div>
-      <p>Hello world</p>
+    <div className={styles.list}>
+      <ul>{renderedCurrencies}</ul>
     </div>
   );
 };
