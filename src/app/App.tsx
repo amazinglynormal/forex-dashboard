3b9458ea15856497700d7f924f09c9e37b24000e
Stephen <stephenbrady365@gmail.com>
2021-05-28 12:31:10 +0100

    Update <App> to fetch initial data

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,22 +1,83 @@
+import { useState, useEffect } from "react";
+
 import { Header } from "../components/Header";
 import { Layout } from "../components/Layout";
 import { Converter } from "../converter/Converter";
 import { Graph } from "../graph/Graph";
 import { RateList } from "../rates-list/RateList";
-import useRateData from "../hooks/useRateData";
+
+import fetchCurrencies from "../utils/fetchCurrencies";
+import fetchLatestExchangeRate from "../utils/fetchLatestExchangeRate";
+import fetchPreviousExchangeRate from "../utils/fetchPreviousExchangeRate";
+
+import { Currencies } from "../interfaces/Currencies.interface";
+import { LatestRate } from "../interfaces/LatestRate.interface";
+import { PreviousRate } from "../interfaces/PreviousRate.interface";
+
+interface State {
+  currencies: Currencies;
+  latest: LatestRate;
+  previous: PreviousRate;
+}
+
+const initialData = {
+  currencies: {},
+  latest: {
+    amount: 0,
+    base: "EUR",
+    date: "2020-01-01",
+    rates: {
+      EUR: 0,
+    },
+  },
+  previous: {
+    amount: 0,
+    base: "EUR",
+    date: "2020-01-01",
+    rates: {
+      EUR: 0,
+    },
+  },
+};
 
 function App() {
-  const { rateListData, latestRateData, currencyData } = useRateData();
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState<State>(initialData);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const currencies = await fetchCurrencies();
+      const latest = await fetchLatestExchangeRate();
+      const previous = await fetchPreviousExchangeRate();
+
+      if (currencies.data && latest.data && previous.data) {
+        setData({
+          currencies: currencies.data,
+          latest: latest.data,
+          previous: previous.data,
+        });
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <>
       <Header headingSize="h1" headingText="Forex Dashboard" />
       <Layout>
-        <RateList rateListData={rateListData} />
-        <Graph />
+        <RateList
+          latestData={data.latest}
+          previousData={data.previous}
+          currencies={data.currencies}
+          loading={loading}
+        />
+        <Graph currencies={data.currencies} loading={loading} />
         <Converter
-          latestRateData={latestRateData.data}
-          currencies={currencyData.data}
+          latestData={data.latest}
+          currencies={data.currencies}
+          loading={loading}
         />
       </Layout>
     </>
