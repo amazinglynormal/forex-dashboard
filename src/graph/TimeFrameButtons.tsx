7f0da29a3a3581c9f668b69460c316c66405adfe
Stephen <stephenbrady365@gmail.com>
2021-05-29 10:46:32 +0100

    Add methods to calculate dates

--- src/graph/TimeFrameButtons.tsx
+++ src/graph/TimeFrameButtons.tsx
@@ -5,11 +5,71 @@ interface Props {
 }
 
 export const TimeFrameButtons = ({ onClickHandler }: Props) => {
+  const calculateTenYearsAgo = () => {
+    const today = new Date();
+
+    const date = today.getDate();
+    const month = today.getMonth() + 1;
+    const year = today.getFullYear() - 10;
+
+    return `${year}-${month < 10 ? "0" : ""}${month}-${
+      date < 10 ? "0" : ""
+    }${date}..`;
+  };
+
+  const calculateOneYearAgo = () => {
+    const today = new Date();
+
+    const date = today.getDate();
+    const month = today.getMonth() + 1;
+    const year = today.getFullYear() - 1;
+
+    return `${year}-${month < 10 ? "0" : ""}${month}-${
+      date < 10 ? "0" : ""
+    }${date}..`;
+  };
+
+  const calculateOneMonthAgo = () => {
+    const today = new Date();
+
+    const date = today.getDate();
+    let month = today.getMonth();
+    const year = today.getFullYear();
+
+    if (month === 0) {
+      month = 12;
+    }
+    return `${year}-${month < 10 ? "0" : ""}${month}-${
+      date < 10 ? "0" : ""
+    }${date}..`;
+  };
+
   return (
     <div className={styles.buttonContainer}>
-      <button onClick={() => onClickHandler("2021-04-01..")}>1 month</button>
-      <button onClick={() => onClickHandler("2020-05-01..")}>1 year</button>
-      <button onClick={() => onClickHandler("2010-01-01..")}>10 years</button>
+      <button
+        onClick={() => {
+          const date = calculateOneMonthAgo();
+          onClickHandler(date);
+        }}
+      >
+        1 month
+      </button>
+      <button
+        onClick={() => {
+          const date = calculateOneYearAgo();
+          onClickHandler(date);
+        }}
+      >
+        1 year
+      </button>
+      <button
+        onClick={() => {
+          const date = calculateTenYearsAgo();
+          onClickHandler(date);
+        }}
+      >
+        10 years
+      </button>
       <button onClick={() => onClickHandler("2000-01-01..")}>Since 2000</button>
     </div>
   );
